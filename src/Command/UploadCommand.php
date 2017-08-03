74c1096c0dbb120ae22df76fb384e82f9558be87
MaDnh <dodanhmanh@gmail.com>
2017-08-03 12:16:28 +0700

    Update command, composer, dependencies

--- src/Command/UploadCommand.php
+++ src/Command/UploadCommand.php
@@ -5,12 +5,9 @@ namespace MaDnh\LaravelUpload\Command;
 
 
 use Illuminate\Console\Command;
-use MaDnh\LaravelCommandUtil\CommandUtil;
 
 class UploadCommand extends Command
 {
-    use CommandUtil;
-
     protected $signature = 'upload:clear';
     protected $description = 'Clear uploaded temporary files';
 
@@ -18,41 +15,81 @@ class UploadCommand extends Command
     {
         $this->banner('Clear Uploaded Temporary Files');
 
+        $keep_from = time() - (int)config('upload.temporary_live_time', 86400);
+        $files = $this->getFiles($keep_from);
 
-        $upload_temp_path = config('upload.upload_temp_path', storage_path('app/upload_temp'));
-        $upload_temp_path = rtrim($upload_temp_path, '\\/') . DIRECTORY_SEPARATOR;
-        $upload_temp_path_length = strlen($upload_temp_path);
-        $temp_files = glob($upload_temp_path . '*', GLOB_NOSORT);
-        $live_time = (int)config('upload.temporary_live_time', 86400);
-        $keep_from = time() - $live_time;
-        $delete_total = 0;
-        $chars = ['deleted' => "<fg=blue>\xF0\x9F\x97\xB8</>", 'failed' => "<fg=red>\xF0\x9F\x97\x99</>"];
+        $this->info('Found <comment>' . count($files) . '</comment> file(s) before <comment>' . date('Y-m-d H:i:s', $keep_from) . "</comment>\n");
 
-        $this->info('Found <comment>' . count($temp_files) . '</comment> file(s) before <comment>' . date('Y-m-d H:i:s', $keep_from) . '</comment>');
+        $deleted = !empty($files) ? $this->deleteFiles($files) : 0;
 
-        foreach ($temp_files as $temp_file) {
-            $filename = substr($temp_file, $upload_temp_path_length);
-            $filenamePad = str_pad($filename, 60, ' ', STR_PAD_RIGHT) . str_repeat(' ', 5);
+        $this->info("\n\nComplete, removed <comment>" . $deleted . '</comment> file(s)');
+    }
 
-            try {
-                $last_mod = filemtime($temp_file);
+    /**
+     * Get files to remove
+     * @param int $expireAt While before this timestamp will be remove
+     * @return array
+     */
+    protected function getFiles($expireAt)
+    {
+        $upload_temp_path = rtrim(config('upload.upload_temp_path', storage_path('app/upload_temp')), '\\/') . DIRECTORY_SEPARATOR;
+        $founds = glob($upload_temp_path . '*', GLOB_NOSORT);
+        $files = [];
 
-                if (!$last_mod || $last_mod < $keep_from) {
-                    if (rand(0, 10) < 5) {
-                        throw new \Exception('Random delete failed reason (haha)');
-                    }
-                    if (!\File::delete($temp_file)) {
-                        throw new \Exception('Unknown');
-                    }
+        foreach ($founds as $found) {
+            $last_mod = filemtime($found);
 
-                    $this->line($this->getListIndex() . $chars['deleted'] . '  ' . $filenamePad);
-                    $delete_total++;
-                }
-            } catch (\Exception $e) {
-                $this->warn($this->getListIndex() . $chars['failed'] . '  ' . $filenamePad . " <error>Error</error> " . $e->getMessage());
+            if (!$last_mod || $last_mod < $expireAt) {
+                $files[] = $found;
             }
         }
 
-        $this->info("\n\nComplete, removed <comment>" . $delete_total . '</comment> file(s)');
+        return $files;
+    }
+
+    /**
+     * @param $files
+     * @return int Deleted count
+     */
+    protected function deleteFiles($files)
+    {
+        $deleted = 0;
+        $index = 0;
+        $chars = ['deleted' => "<fg=blue>\xF0\x9F\x97\xB8</>", 'failed' => "<fg=red>\xF0\x9F\x97\x99</>"];
+
+        foreach ($files as $temp_file) {
+            $filename = basename($temp_file);
+            $filenamePad = str_pad($filename, 60, ' ', STR_PAD_RIGHT) . str_repeat(' ', 5);
+            $fileIndex = ' <info>' . str_pad(++$index . '', '3', ' ', STR_PAD_LEFT) . '.</info>  ';
+
+            try {
+                if (!\File::delete($temp_file)) {
+                    throw new \Exception('Unknown');
+                }
+
+                $this->line($fileIndex . $chars['deleted'] . '  ' . $filenamePad);
+                $deleted++;
+            } catch (\Exception $e) {
+                $this->warn($fileIndex . $chars['failed'] . '  ' . $filenamePad . " <error>Error</error> " . $e->getMessage());
+            }
+        }
+
+        return $deleted;
+    }
+
+    protected function banner($message)
+    {
+        $messSize = strlen($message);
+        $pad = 5;
+        $lines = [];
+
+        $lines[] = "\n";
+        $lines[] = "\n";
+        $lines[] = '<info>' . str_repeat(' ', $pad) . $message . '</info>';
+        $lines[] = "\n";
+        $lines[] = '<fg=yellow>' . str_repeat('#', $messSize + $pad * 2) . '</>';
+        $lines[] = "\n";
+
+        $this->line(implode("\n", $lines));
     }
 }
